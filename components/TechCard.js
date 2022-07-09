58ecae402b33341f4e92cb1c0b7d661f88b630b4
Matthew Sweet <sweet@andrews.edu>
2022-07-09 06:44:14 -0500

    Updated icon sizes for better responsiveness

--- components/TechCard.js
+++ components/TechCard.js
@@ -7,7 +7,7 @@ export default function TechCard(props) {
         className={`p-1 text-zinc-900 w-full sm:max-w-xl text-center mb-4 sm:text-left sm:mb-0 ${props.background} duration-300 select-none animate-fade-in`}
       >
         <div className="flex text-xs sm:text-sm tracking-tight">
-          <div className="flex items-center justify-center p-6 text-7xl sm:text-[25ch] bg-zinc-900 w-full min-h-[3ch]">
+          <div className="flex items-center justify-center p-6 text-7xl sm:text-[10ch] lg:text-[20ch] 2xl:text-[25ch] bg-zinc-900 w-full min-h-[3ch]">
             <i className={`${props.icon} ${props.iconColor} text-center`}></i>
           </div>
         </div>
